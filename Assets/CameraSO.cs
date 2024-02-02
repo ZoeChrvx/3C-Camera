0bc0dfb9b11e7c12a715095b25eab0b7fb548c2e
ZoeChrvx <zchervaux@artfx.fr>
2024-02-02 15:58:42 +0100

    [add] Scriptable Object + Camera Settings code

--- Assets/CameraSO.cs
+++ Assets/CameraSO.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu]
 public class CameraSO : ScriptableObject
 {
+    [Header("General")]
     public Vector3 offsetPosition;
     public GameObject pivotPoint;
     public float rotationSpeed, mouvementSpeed;
+    [Space]
+
+    [Header("Fov and Zoom")]
     [Range(20, 180)] //slider
     public float fov;
     public float fovZoom;
     public Vector3 positionZoom;
+    [Space]
+
+    [Header("Collision and Perspective")]
     public bool checkCollision;
     public float clipping;
     public bool isPerspective;
---
+++ Assets/CameraSettings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSettings : MonoBehaviour
+{
+    public CameraSO cameraSOFPS, cameraSOTPS, cameraSOScroller;
+    
+    public void
+
+
+}
